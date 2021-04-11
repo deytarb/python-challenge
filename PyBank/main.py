979d2c8c643386632d8f0579f353ebbbc11d2eb4
deytarb <55323523+deytarb@users.noreply.github.com>
2021-04-11 16:25:04 -0400

    new

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,48 @@
-print("Hello")
+
+import os
+import csv
+from pathlib import Path 
+
+# Locate data
+input = Path("Resources", "budget_data.csv")
+
+# Where will be located the data
+totalbymonths = []
+allprofit = []
+variationbymonth = []
+ # Open csv file
+with open(input,newline="", encoding="utf-8") as forecast:
+
+    csvreader = csv.reader(forecast,delimiter=",") 
+   
+    header = next(csvreader)  
+
+    # Look up each rows in the data
+    for row in csvreader: 
+      # Add total profit
+        totalbymonths.append(row[0])
+        allprofit.append(int(row[1]))
+
+    # Goes for all to get the monthly changes
+    for i in range(len(allprofit)-1):
+        
+        variationbymonth.append(allprofit[i+1]-allprofit[i])
+        
+# Identify the maximum of increase and minimum 
+maximumincrease = max(variationbymonth)
+minimumdecrease = min(variationbymonth)
+
+# Identify the maximum of increase and minimum by month
+maximumincreasemonth = variationbymonth.index(max(variationbymonth)) + 1
+maximumdecreasemonth = variationbymonth.index(min(variationbymonth)) + 1 
+
+# Final results
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {len(totalbymonths)}")
+print(f"Total: ${sum(allprofit)}")
+print(f"Average Change: {round(sum(variationbymonth)/len(variationbymonth),2)}")
+print(f"Greatest Increase in Profits: {totalbymonths[maximumincreasemonth]} (${(str(maximumincrease))})")
+print(f"Greatest Decrease in Profits: {totalbymonths[maximumdecreasemonth]} (${(str(minimumdecrease))})")
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,96 +1,63 @@
-# Import dependencies
-import os, csv
+
+import os
+import csv
 from pathlib import Path 
 
-# Assign file location with the pathlib library
-csv_file_path = Path("python-challenge", "PyPoll", "election_data.csv")
+# Where is the path
+path = Path("Resources","election_data.csv")
 
-# Declare Variables 
-total_votes = 0 
-khan_votes = 0
-correy_votes = 0
-li_votes = 0
-otooley_votes = 0
+# Assign variables to store data
+votesall = 0 
+khan = 0
+correy = 0
+li = 0
+otooley = 0
 
-# Open csv in default read mode with context manager
-with open(csv_file_path,newline="", encoding="utf-8") as elections:
+# Bring csv file
+with open(path,newline="", encoding="utf-8") as elections:
+    csv = csv.reader(elections,delimiter=",") 
 
-    # Store data under the csvreader variable
-    csvreader = csv.reader(elections,delimiter=",") 
+    # Look up in the header
+    header = next(csv)     
 
-    # Skip the header so we iterate through the actual values
-    header = next(csvreader)     
+    # Go over over all rows
+    for row in csv: 
 
-    # Iterate through each row in the csv
-    for row in csvreader: 
+        votesall +=1
 
-        # Count the unique Voter ID's and store in variable  called total_votes
-        total_votes +=1
-
-        # We have four candidates if the name is found, count the times it appears and store in a list
-        # We can use this values in our percent vote calculation in the print statements
+        # How many times each candidate appear
         if row[2] == "Khan": 
-            khan_votes +=1
+            khan +=1
         elif row[2] == "Correy":
-            correy_votes +=1
+            correy +=1
         elif row[2] == "Li": 
-            li_votes +=1
+            li +=1
         elif row[2] == "O'Tooley":
-            otooley_votes +=1
+            otooley +=1
 
- # To find the winner we want to make a dictionary out of the two lists we previously created 
-candidates = ["Khan", "Correy", "Li","O'Tooley"]
-votes = [khan_votes, correy_votes,li_votes,otooley_votes]
+ # create a dictionay inluding  candidates
+candidate = ["Khan", "Correy", "Li","O'Tooley"]
+No_votes = [khan, correy,li,otooley]
 
-# We zip them together the list of candidate(key) and the total votes(value)
-# Return the winner using a max function of the dictionary 
-dict_candidates_and_votes = dict(zip(candidates,votes))
+# Getting the winner 
+dict_candidates_and_votes = dict(zip(candidate,No_votes))
 key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
 
-# Print a the summary of the analysis
-khan_percent = (khan_votes/total_votes) *100
-correy_percent = (correy_votes/total_votes) * 100
-li_percent = (li_votes/total_votes)* 100
-otooley_percent = (otooley_votes/total_votes) * 100
+# Summary
+khan_percent = round((khan/votesall) * 100)
+correy_percent = round((correy/votesall) * 100)
+li_percent = round((li/votesall)* 100)
+otooley_percent = round((otooley/votesall) * 100)
 
-# Print the summary table
+# Results
 print(f"Election Results")
 print(f"----------------------------")
-print(f"Total Votes: {total_votes}")
+print(f"Total Votes: {votesall}")
 print(f"----------------------------")
-print(f"Khan: {khan_percent:.3f}% ({khan_votes})")
-print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
-print(f"Li: {li_percent:.3f}% ({li_votes})")
-print(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
+print(f"Khan: {khan_percent:.3f}% ({khan})")
+print(f"Correy: {correy_percent:.3f}% ({correy})")
+print(f"Li: {li_percent:.3f}% ({li})")
+print(f"O'Tooley: {otooley_percent:.3f}% ({otooley})")
 print(f"----------------------------")
 print(f"Winner: {key}")
 print(f"----------------------------")
-
-# Output files
-# Assign output file location and with the pathlib library
-output_file = Path("python-challenge", "PyPoll", "Election_Results_Summary.txt")
-
-with open(output_file,"w") as file:
-
-# Write methods to print to Elections_Results_Summary 
-    file.write(f"Election Results")
-    file.write("\n")
-    file.write(f"----------------------------")
-    file.write("\n")
-    file.write(f"Total Votes: {total_votes}")
-    file.write("\n")
-    file.write(f"----------------------------")
-    file.write("\n")
-    file.write(f"Khan: {khan_percent:.3f}% ({khan_votes})")
-    file.write("\n")
-    file.write(f"Correy: {correy_percent:.3f}% ({correy_votes})")
-    file.write("\n")
-    file.write(f"Li: {li_percent:.3f}% ({li_votes})")
-    file.write("\n")
-    file.write(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
-    file.write("\n")
-    file.write(f"----------------------------")
-    file.write("\n")
-    file.write(f"Winner: {key}")
-    file.write("\n")
-    file.write(f"----------------------------")
